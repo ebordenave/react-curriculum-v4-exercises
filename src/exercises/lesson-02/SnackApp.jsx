28c77925b91c9d30b40b4d3529187eb8e2f5d253
Edward Bordenave <edward.bordenave@gmail.com>
2026-04-12 10:22:48 -0400

    Lesson 02 exercise (#2)
    * lesson-02-exercise
    
    * add screenshot
    
    * remove old image, add new image

---
+++ src/exercises/lesson-02/SnackApp.jsx
@@ -0,0 +1,15 @@
+import SnackFooter from './SnackFooter';
+import SnackHeader from './SnackHeader';
+import SnackList from './SnackList';
+
+function SnackApp() {
+  return (
+    <div>
+      <SnackHeader />
+      <SnackList />
+      <SnackFooter />
+    </div>
+  );
+}
+
+export default SnackApp;
