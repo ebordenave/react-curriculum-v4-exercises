6e2216aec5f613b1851740243c7e125725b76c2f
ebordenave <edward.bordenave@gmail.com>
2026-04-26 18:54:28 -0400

    fix BugEventPropagation

--- src/exercises/lesson-04/FindCorrectHook.jsx
+++ src/exercises/lesson-04/FindCorrectHook.jsx
@@ -1,16 +1,20 @@
 // TOPIC: Choose the correct tool: useRef vs useState
+
+import { useState } from 'react';
+
 // TASK: Make sure it updates the text *without* triggering a re-render
 export default function FindCorrectHook() {
-  let clickCount = 0; // ← incorrect implementation
+  //let clickCount = 0; // ← incorrect implementation
+  const [count, setCount] = useState(0);
 
   function handleClick() {
-    clickCount++;
+    setCount((prevCount) => prevCount + 1);
   }
 
   return (
     <div>
       <h2>useRef vs useState Decision</h2>
-      <button onClick={handleClick}>{clickCount} Clicks</button>
+      <button onClick={handleClick}>{count} Clicks</button>
     </div>
   );
 }
