50ba4a2f48a87a1750b037fff4c84a0a29498e0d
ebordenave <edward.bordenave@gmail.com>
2026-04-26 19:18:19 -0400

    fix FillRefFocus

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,14 +1,28 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+// useRef is a React Hook that lets you reference a value that’s not
+// needed for rendering.
+
+import { useRef } from 'react';
+
 export default function FillRefFocus() {
-  function focusInput() {}
+  // source: react.dev documentation
+  const ref = useRef(null);
+
+  // React will set the current property of your ref object to that DOM node.
+  function focusInput() {
+    ref.current.focus();
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
-
-      <input type="text" placeholder="Type here..." />
-
+      {/* <input ref={nameRef} type="text" name="name" id="name"/> */}
+      <input
+        ref={ref}
+        type="text"
+        placeholder="Type here..."
+      />
       <button onClick={focusInput}>Focus Input</button>
     </div>
   );
